1e949aad66d4a1d9f727e3d6087e4b717f9a0b99
Tech-sis <abilawonadeola@gmail.com>
2021-11-29 13:48:52 +0100

    updated package.json

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/fixture" element={<Fixture />} />
       </Routes>
